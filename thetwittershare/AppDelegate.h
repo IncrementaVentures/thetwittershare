465eae865cd8601aef1a6dfd49174b31a07af255
Mario Merino <Mario@marios-macbook.local>
2015-11-02 07:04:37 +0100

    Initial Commit

---
+++ thetwittershare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  thetwittershare
+//
+//  Created by Mario Merino on 11/2/15.
+//  Copyright © 2015 Mario Merino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
